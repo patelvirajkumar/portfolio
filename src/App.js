c7b4a9d04c560f8721213e3e35e4466649266341
virajkumar patel <virajkumarpatel@virajkumars-MBP.phub.net.cable.rogers.com>
2021-03-18 14:50:03 -0400

    Navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from '../src/components/Navbar/Navbar'
+
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   </>
+  );
+}
+
+export default App;
